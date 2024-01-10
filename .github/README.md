e0baec252a6f860e7718c1ee7b5f6243a446b6cd
wallsified <danielparedes@ciencias.unam.mx>
2024-01-10 13:33:41 -0600

    hello

---
+++ .github/README.md
@@ -0,0 +1,35 @@
+<h2 align='center'>
+ Go with the (nord)flow...
+</h2>
+
+<br>
+
+My Nord Remix on the Adaptive Theme for [Archcraft](https://archcraft.io/). 
+
+## The System Setup
+- **OS** - Archcraft
+- **WM** - Openbox
+- **Term** - Alacritty
+- **Comp** - Picom
+- **Menu/Launcher/Window Switcher** - Rofi
+- **Shell** - Zsh w/ Powerlevel10k + Colorls
+- **Music Player** - Spicetify w/Sleek Nord Theme
+- **Wallpapers** Originally found in the [Nordic Wallpapers Repo](https://github.com/linuxdotexe/nordic-wallpapers)
+
+## The Browser Setup
+
+- **Browser** - Brave Beta w/ [Nord Theme](https://chromewebstore.google.com/detail/nord-theme/dhlnjfhjjbminbjbegeiijdakdkamjoi)
+- **Extensions** - [Notion Enhancer](https://github.com/notion-enhancer/notion-enhancer) + [Notion Themes](https://github.com/notionblog/NotionThemes)
+
+## Dependencies
+
+```sh
+sudo pacman -Syu alacritty playerctl openbox nerd-fonts-jetbrains-mono ttf-jetbrains-mono brightnessctl networkmanager alsa-utils alsa-plugins alsa-firmware zsh betterlockscreen thunar vlc pfetch
+```
+
+Note: List is a wip. Still adding to this
+
+## A look into the nordflow
+
+## Some keybinds
+
---
+++ openbox/themes/nordflow/theme.bash
@@ -0,0 +1,92 @@
+# ------------------------------------------------------------------------------
+# Copyright (C) 2020-2023 Aditya Shakya <adi1090x@gmail.com>
+# Edited by @Wallsified
+# Nordflow Theme
+# ------------------------------------------------------------------------------
+
+# Colors
+background='#2E3440'
+foreground='#D8DEE9'
+color0='#4C566A'
+color1='#81A1C1'
+color2='#8FBCBB'
+color3='#A3BE8C'
+color4='#B48EAD'
+color5='#BF616A'
+color6='#ECEFF4'
+color7='#EBCB8B'
+color8='#3B4252'
+color9='#81A1C1'
+color10='#88C0D0'
+color11='#A3BE8C'
+color12='#B48EAD'
+color13='#BF616A'
+color14='#E5E9F0'
+color15='#E5E9F0'
+
+accent='#00BCD4'
+light_value='0.15'
+dark_value='0.30'
+
+# Wallpaper
+wdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+wallpaper="$wdir/wallpaper"
+
+# Polybar
+polybar_font='Iosevka Nerd Font:size=10;3'
+
+# Rofi
+rofi_font='Iosevka 10'
+rofi_icon='Papirus-Apps'
+
+# Terminal
+terminal_font_name='JetBrainsMono Nerd Font'
+terminal_font_size='10'
+
+# Geany
+geany_colors='adapta.conf'
+geany_font='JetBrains Mono 10'
+
+# Appearance
+gtk_font='Noto Sans 9'
+gtk_theme='Adapta-Nokto'
+icon_theme='Nordzy-cyan-dark'
+cursor_theme='Vimix'
+
+# Openbox
+ob_theme='Adapta-Nokto'
+ob_layout='MLC'
+ob_font='JetBrains Mono'
+ob_font_size='9'
+ob_menu='menu-icons.xml'
+ob_margin_t='44'
+ob_margin_b='10'
+ob_margin_l='10'
+ob_margin_r='10'
+
+# Dunst
+dunst_width='450'
+dunst_height='200'
+dunst_offset='10x44'
+dunst_origin='top-right'
+dunst_font='Iosevka Custom 9'
+dunst_border='1'
+dunst_separator='2'
+
+# Plank
+plank_hmode='auto'
+plank_offset='0'
+plank_position='bottom'
+plank_theme='Transparent'
+plank_icon_size='32'
+plank_zoom_percent='120'
+
+# Picom
+picom_backend='glx'
+picom_corner='10'
+picom_shadow_r='14'
+picom_shadow_o='0.30'
+picom_shadow_x='-12'
+picom_shadow_y='-12'
+picom_blur_method='none'
+picom_blur_strength='0'
